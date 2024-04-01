d1658633061c928cba8c79300047e36bbcb7bae1
hai <7210hai@gmail.com>
2024-04-01 21:07:42 +0700

    s 3

--- app/src/main/java/com/example/customApp/MainActivity.kt
+++ app/src/main/java/com/example/customApp/MainActivity.kt
@@ -6,22 +6,26 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.example.customApp.data.OfflinesenDataRepository
+import com.example.customApp.data.sensorDataDatabase
+import com.example.customApp.ui.home.DashBoard
+import com.example.customApp.ui.home.DashBoardViewModel
 import com.example.customApp.ui.theme.MyApplicationTheme
 
-class MainActivity : ComponentActivity() { // Update to ComponentActivity
-//    private lateinit var viewModel: SensorDataViewModel
-
+class MainActivity : ComponentActivity() {
+    private lateinit var viewModel: DashBoardViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Initialize ViewModel
-//        viewModel = ViewModelProvider(this).get(SensorDataViewModel::class.java)
-
+        val sensorDataDao = sensorDataDatabase.getDatabase(this).sensorDataDao()
+        val repository = OfflinesenDataRepository(sensorDataDao)
+        viewModel = DashBoardViewModel(repository) // Initialize your ViewModel
         setContent {
             MyApplicationTheme {
                 // Surface is the top-most composable where content will be rendered.
                 Surface(modifier = Modifier.fillMaxSize()) {
-//                    SensorDataScreen(viewModel = viewModel)
+                    // Uncomment and pass necessary view models or data to SensorDataScreen
+                    DashBoard(viewModel = viewModel)
                 }
             }
         }
--- app/src/main/java/com/example/customApp/ui/home/DashBoard.kt
+++ app/src/main/java/com/example/customApp/ui/home/DashBoard.kt
@@ -1,13 +1,32 @@
 package com.example.customApp.ui.home
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import com.customApp.R
+import com.example.customApp.data.sensorData
+import androidx.compose.material3.Text
+import androidx.compose.ui.unit.dp
+
 
 @Composable
 fun DashBoard(viewModel: DashBoardViewModel) {
@@ -18,3 +37,5 @@ fun DashBoard(viewModel: DashBoardViewModel) {
         Text(text = "Number of items: ${uiState.value.itemList.size}")
     }
 }
+
+
